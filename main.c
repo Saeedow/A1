21c24eafaf435d5b830f714936952dd4048c12cc
AbasAli03 <125214662+AbasAli03@users.noreply.github.com>
2023-09-13 14:34:13 +0200

    convertion

--- main.c
+++ main.c
@@ -1,74 +1,96 @@
-//To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-//To run (linux/mac): ./main.out example.bmp example_inv.bmp
+// To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
+// To run (linux/mac): ./main.out example.bmp example_inv.bmp
 
-//To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+// To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
+// To run (win): main.exe example.bmp example_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "cbmp.h"
 
-const int TH = 90;
+const int TH = 120;
 
-//Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-    
-      output_image[x][y] =  (input_image[x][y][0]+input_image[x][y][1]+input_image[x][y][2])/3;
-      
-    }
-  }
-}
-
-void convertToBinary(unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]){
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      if(output_image[x][y]<=TH){
-        output_image[x][y]=0;
-      }else{
-       output_image[x][y]=255;
-      }
-      
-      
-    }
-  }
-}
-
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH];
-
-//Main function
-int main(int argc, char** argv)
+// Function to invert pixels of an image (negative)
+void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
 {
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      output_image[x][y][0] = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+      output_image[x][y][1] = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+      output_image[x][y][2] = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+    }
+  }
+}
 
-  //Checking that 2 arguments are passed
+void convertToBinary(unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      if (output_image[x][y] <= TH)
+      {
+        output_image[x][y][0] = 0;
+        output_image[x][y][1] = 0;
+        output_image[x][y][2] = 0;
+      }
+      else
+      {
+        output_image[x][y][0] = 255;
+        output_image[x][y][1] = 255;
+        output_image[x][y][2] = 255;
+      }
+    }
+  }
+}
+void converTo3D(unsigned char twoD[BMP_WIDTH][BMP_HEIGTH], unsigned char threeD[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+
+      threeD[x][y][0] = twoD[x][y];
+      threeD[x][y][1] = twoD[x][y];
+      threeD[x][y][2] = twoD[x][y];
+    }
+  }
+}
+
+// Declaring the array to store the image (unsigned char = unsigned 8 bit)
+unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH];
+
+// Main function
+int main(int argc, char **argv)
+{
+  // argc counts how may arguments are passed
+  // argv[0] is a string with the name of the program
+  // argv[1] is the first command line argument (input image)
+  // argv[2] is the second command line argument (output image)
+
+  // Checking that 2 arguments are passed
   if (argc != 3)
   {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+    exit(1);
   }
 
   printf("Example program - 02132 - A1\n");
 
-  //Load image from file
+  // Load image from file
   read_bitmap(argv[1], input_image);
 
-  //Run inversion
-  invert(input_image,output_image);
+  // Run inversion
+  invert(input_image, output_image);
 
   convertToBinary(output_image);
 
-  //Save image to file
+  // converTo3D(output_image, input_image);
+
+  // Save image to file
   write_bitmap(output_image, argv[2]);
 
   printf("Done!\n");
