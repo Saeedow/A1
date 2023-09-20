b41019b0f9c02acc3611e24d5cadd98a31b4fd9d
AbasAli03 <125214662+AbasAli03@users.noreply.github.com>
2023-09-20 16:39:57 +0200

    erode and cell detection

--- main.c
+++ main.c
@@ -1,64 +1,69 @@
-// To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-// To run (linux/mac): ./main.out example.bmp example_inv.bmp
+// To compile (linurow/mac): gcc cbmp.c main.c -o main.out -std=c99
+// To run (linurow/mac): ./main.out erowample.bmp erowample_inv.bmp
 
-// To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-// To run (win): main.exe example.bmp example_inv.bmp
+// To compile (win): gcc cbmp.c main.c -o main.erowe -std=c99
+// To run (win): main.erowe erowample.bmp erowample_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "cbmp.h"
 
 const int TH = 120;
+// Declaring the arracol to store the image (unsigned char = unsigned 8 bit)
+unsigned char iinput_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH];
 
-// Function to invert pixels of an image (negative)
+// Function to invert pirowels of an image (negative)
 void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH])
 {
-  for (int x = 0; x < BMP_WIDTH; x++)
+  for (int row = 0; row < BMP_WIDTH; row++)
   {
-    for (int y = 0; y < BMP_HEIGTH; y++)
+    for (int col = 0; col < BMP_HEIGTH; col++)
     {
-      output_image[x][y] = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+      output_image[row][col] = (input_image[row][col][0] + input_image[row][col][1] + input_image[row][col][2]) / 3;
     }
   }
 }
 
-void convertToBinary(unsigned char output_image[BMP_WIDTH][BMP_HEIGTH])
+void convertToBinarcol(unsigned char output_image[BMP_WIDTH][BMP_HEIGTH])
 {
-  for (int x = 0; x < BMP_WIDTH; x++)
+  for (int row = 0; row < BMP_WIDTH; row++)
   {
-    for (int y = 0; y < BMP_HEIGTH; y++)
+    for (int col = 0; col < BMP_HEIGTH; col++)
     {
-      if (output_image[x][y] <= TH)
+      if (output_image[row][col] <= TH)
       {
-        output_image[x][y] = 0;
+        output_image[row][col] = 0;
       }
       else
       {
-        output_image[x][y] = 255;
+        output_image[row][col] = 255;
       }
     }
   }
 }
+
 void converTo3D(unsigned char twoD[BMP_WIDTH][BMP_HEIGTH], unsigned char threeD[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
 {
-  for (int x = 0; x < BMP_WIDTH; x++)
+  for (int row = 0; row < BMP_WIDTH; row++)
   {
-    for (int y = 0; y < BMP_HEIGTH; y++)
+    for (int col = 0; col < BMP_HEIGTH; col++)
     {
 
-      threeD[x][y][0] = twoD[x][y];
-      threeD[x][y][1] = twoD[x][y];
-      threeD[x][y][2] = twoD[x][y];
+      threeD[row][col][0] = twoD[row][col];
+      threeD[row][col][1] = twoD[row][col];
+      threeD[row][col][2] = twoD[row][col];
     }
   }
 }
+
 int isCompleted(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH])
 {
-  for (int x = 0; x < BMP_WIDTH; x++)
+  for (int row = 0; row < BMP_WIDTH; row++)
   {
-    for (int y = 0; y < BMP_HEIGTH; y++)
+    for (int col = 0; col < BMP_HEIGTH; col++)
     {
-      if (input_image[x][y] == 255)
+      if (input_image[row][col] == 255)
       {
         return 1;
       }
@@ -66,39 +71,180 @@ int isCompleted(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH])
   }
   return 0;
 }
-void erode(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH])
+
+void generateOutputImage(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int row, int col)
 {
 
-  int counter = 1;
-  do
+  for (int i = 0; i < 5; i++)
   {
-    for (int x = 1; x < BMP_WIDTH - 1; x++)
+    input_image[row + i][col][0] = 255;
+    input_image[row + i][col][1] = 0;
+    input_image[row + i][col][2] = 0;
+    input_image[row - i][col][0] = 255;
+    input_image[row - i][col][1] = 0;
+    input_image[row - i][col][2] = 0;
+    input_image[row][col + i][0] = 255;
+    input_image[row][col + i][1] = 0;
+    input_image[row][col + i][2] = 0;
+    input_image[row][col - i][0] = 255;
+    input_image[row][col - i][1] = 0;
+    input_image[row][col - i][2] = 0;
+  }
+}
+
+void cellDetection(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH])
+{
+
+  for (int row = 0; row < BMP_HEIGTH; row++)
+  {
+    for (int col = 0; col < BMP_WIDTH; col++)
     {
-      for (int y = 1; y < BMP_HEIGTH - 1; y++)
+      int whiteCount = 0;
+      int blackCount = 0;
+
+      if (input_image[row][col] == 255)
       {
 
-        if (input_image[x + 1][y] == 255 && input_image[x - 1][y] == 255 && input_image[x][y + 1] == 255 && input_image[x][y - 1] == 255 && input_image[x][y - 1] == 255)
+        for (int i = -6; i < 6; i++)
         {
-          input_image[x][y] = 0;
+          for (int j = -6; j < 6; j++)
+          {
+            if (row + i >= 0 && row + i < BMP_HEIGTH && col + j >= 0 && col + j < BMP_WIDTH)
+            {
+              if (input_image[row + i][col + j] == 255)
+              {
+                input_image[row + i][col + j] == 0;
+                input_image[row][col] == 0;
+                whiteCount++;
+              }
+            }
+          }
+        }
+
+        if (whiteCount > 0)
+        {
+          int exclusionBlack = 0;
+
+          if (row)
+
+            if (exclusionBlack == 52)
+            {
+              generateOutputImage(iinput_image, row, col);
+              exclusionBlack = 0;
+            }
+        }
+      }
+    }
+  }
+}
+
+void erode(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH])
+{
+  int counter = 0;
+  while (isCompleted(input_image))
+  {
+    for (int row = 0; row < BMP_WIDTH; row++)
+    {
+      for (int col = 0; col < BMP_HEIGTH; col++)
+      {
+        // top left
+        if (input_image[row][col] == 255)
+        {
+          if (row == 0 && col == 0)
+          {
+            if (input_image[row + 1][col] == 0 || input_image[row][col + 1] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+          // top right
+          if (row == 0 && col == BMP_WIDTH)
+          {
+            if (input_image[row + 1][col] == 0 || input_image[row][col - 1] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+          // bottom left
+          if (row == BMP_HEIGTH && col == 0)
+          {
+            if (input_image[row - 1][col] == 0 || input_image[row][col + 1] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+          // bottom right
+          if (row == BMP_HEIGTH && col == BMP_WIDTH)
+          {
+            if (input_image[row - 1][col] == 0 || input_image[row][col - 1] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+
+          // first row
+          if (row == 0 && col > 0 && col < BMP_WIDTH)
+          {
+            if (input_image[row + 1][col] == 0 || input_image[row][col - 1] == 0 || input_image[row][col + 1] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+
+          // last row
+
+          if (row == BMP_HEIGTH && col > 0 && col < BMP_WIDTH)
+          {
+            if (input_image[row - 1][col] == 0 || input_image[row][col - 1] == 0 || input_image[row][col + 1] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+
+          // first column
+          if (col == 0 && row > 0 && row < BMP_HEIGTH)
+          {
+            if (input_image[row - 1][col] == 0 || input_image[row][col + 1] == 0 || input_image[row + 1][col] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+          // last column
+          if (col == BMP_HEIGTH && row > 0 && row < BMP_HEIGTH)
+          {
+            if (input_image[row - 1][col] == 0 || input_image[row + 1][col] == 0 || input_image[row][col - 1] == 0)
+            {
+              input_image[row][col] = 0;
+              continue;
+            }
+          }
+
+          if (input_image[row + 1][col] == 0 || input_image[row - 1][col] == 0 || input_image[row][col + 1] == 0 || input_image[row][col - 1] == 0)
+          {
+            input_image[row][col] = 0;
+            continue;
+          }
         }
       }
     }
 
     counter++;
-
-  } while (isCompleted(input_image));
+  }
 
   printf("%d", counter);
 }
 
-// Declaring the array to store the image (unsigned char = unsigned 8 bit)
-unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-unsigned char output_image[BMP_WIDTH][BMP_HEIGTH];
-
 // Main function
 int main(int argc, char **argv)
 {
-  // argc counts how may arguments are passed
+  // argc counts how macol arguments are passed
   // argv[0] is a string with the name of the program
   // argv[1] is the first command line argument (input image)
   // argv[2] is the second command line argument (output image)
@@ -113,18 +259,19 @@ int main(int argc, char **argv)
   printf("Example program - 02132 - A1\n");
 
   // Load image from file
-  read_bitmap(argv[1], input_image);
+  read_bitmap(argv[1], iinput_image);
 
   // Run inversion
-  invert(input_image, output_image);
+  invert(iinput_image, output_image);
 
-  convertToBinary(output_image);
+  convertToBinarcol(output_image);
 
   erode(output_image);
 
-  converTo3D(output_image, input_image);
+  // converTo3D(output_image, input_image);
+
   // Save image to file
-  write_bitmap(input_image, argv[2]);
+  write_bitmap(iinput_image, argv[2]);
 
   printf("Done!\n");
   return 0;
